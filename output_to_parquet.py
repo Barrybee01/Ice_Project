017d2dea5496a3d6cf4cd4ab9988efc3013cd3a6
Barrybee01 <48040764+Barrybee01@users.noreply.github.com>
2025-05-12 22:17:49 -0600

    Add files via upload

---
+++ output_to_parquet.py
@@ -0,0 +1,24 @@
+import pyarrow
+import pandas as pd
+import fastparquet
+
+input_files = ["HOH.txt", "OH1.txt", "OH2.txt"]
+chunk_size = 1000
+delimiter = ' '
+parquet_engine = 'fastparquet'  # using fastparquet since it supports append
+
+for input_file in input_files:
+    output_file = input_file.replace(".txt", ".parquet")
+    first_chunk = True
+
+    for chunk in pd.read_csv(input_file, 
+                             sep=delimiter, 
+                             engine='python', 
+                             chunksize=chunk_size, 
+                             header=0, 
+                             skipinitialspace=True):
+        if first_chunk:
+            chunk.to_parquet(output_file, engine=parquet_engine, index=False)
+            first_chunk = False
+        else:
+            chunk.to_parquet(output_file, engine=parquet_engine, index=False, append=True)
